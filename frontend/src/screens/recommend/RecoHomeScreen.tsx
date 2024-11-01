661cefd26ce6bf95b9c38bf11d2951aef516025c
박지원 <tlsxp13@naver.com>
2024-11-01 10:44:01 +0900

    frontend_dev

---
+++ frontend/src/screens/recommend/RecoHomeScreen.tsx
@@ -0,0 +1,12 @@
+import tw from 'twrnc';
+import { View, Text } from 'react-native';
+
+const RecoHomeScreen = () => {
+ return (
+   <View style={tw``}>
+     <Text>안녕</Text>
+   </View>
+ );
+};
+
+export default RecoHomeScreen;
