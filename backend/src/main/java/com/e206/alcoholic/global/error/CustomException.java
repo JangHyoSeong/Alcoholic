5cda128fb5336ef30e1ef3bd3288e78aff27e523
kdhn97 <ehdgus3726@naver.com>
2024-11-04 11:29:52 +0900

    feat : HTTP error 메시지 생성

--- backend/src/main/java/com/e206/alcoholic/global/error/CustomException.java
+++ backend/src/main/java/com/e206/alcoholic/global/error/CustomException.java
@@ -2,15 +2,10 @@
 package com.e206.alcoholic.global.error;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
-// 커스텀 예외 클래스
 @Getter
+@RequiredArgsConstructor
 public class CustomException extends RuntimeException {
     private final ErrorCode errorCode;
-
-    // ErrorCode enum으로부터 에러 메시지를 받아 예외를 생성
-    public CustomException(ErrorCode errorCode) {
-        super(errorCode.getMessage());
-        this.errorCode = errorCode;
-    }
 }
