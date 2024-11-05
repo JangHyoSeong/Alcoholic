8a545f3807004306ae96038b314ea7966bfbc39f
박지원 <tlsxp13@naver.com>
2024-11-05 17:21:00 +0900

    [FE] refactor : 네비게이션 수정 중

--- frontend/src/navigations/root/RootNavigator.tsx
+++ frontend/src/navigations/root/RootNavigator.tsx
@@ -1,25 +1,17 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import AuthStackNavigator from "../stack/AuthStackNavigator";
 import { useAppStore } from "@/state/useAppStore";
 import MainTabNavigator from "../tab/MainTabNavigator";
 
 const RootNavigator: React.FC = () => {
-  let navigatorToShow: JSX.Element;
   const isLogin = useAppStore((state) => state.isLogin)
-  const setLogin = useAppStore((state) => state.setLogin)
 
-  useEffect(() => {
-    setLogin(isLogin)
-  }, [isLogin, setLogin]);
-
-  if (isLogin) {
-    navigatorToShow = <MainTabNavigator />
-  } else {
-    navigatorToShow = <AuthStackNavigator />
-  }
-
-  return <NavigationContainer>{navigatorToShow}</NavigationContainer>
+  return (
+    <NavigationContainer>
+      {isLogin ? <MainTabNavigator /> : <AuthStackNavigator />}
+    </NavigationContainer>
+  )
 }
 
 export default RootNavigator
