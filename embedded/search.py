de546f1ca8032aa4dce08b7d8ba9ecba4d423e9d
KJH <ever618@naver.com>
2024-11-08 11:21:25 +0900

    feat: 한국어 자음, 모음 분해 기능 구현

--- embedded/search.py
+++ embedded/search.py
@@ -1,17 +1,26 @@
 import re, random
 from fuzzywuzzy import fuzz
+from jamo import h2j, j2hcj
+
 
 def preprocess_text(text):
     text = re.sub(r'[^a-zA-Z0-9가-힣\s]', '', text)
     return text.lower().strip()
 
 
+def text_to_jamo(text):
+    # 한글을 자모 단위로 변환하고, 영어와 숫자는 그대로 반환
+    jamo_text = j2hcj(h2j(text))
+    return jamo_text
+
+
 def calculate_levenshtein_similarity(text1, text2):
-    return fuzz.ratio(text1, text2) / 100
+    custom_text1 = text_to_jamo(preprocess_text(text1))
+    custom_text2 = text_to_jamo(preprocess_text(text2))
+    return fuzz.ratio(custom_text1, custom_text2) / 100
 
 
 def find_most_similar_product(text, product_data):
-    custom_text = preprocess_text(text)
     highest_similarity = 0
     best_match = []
 
