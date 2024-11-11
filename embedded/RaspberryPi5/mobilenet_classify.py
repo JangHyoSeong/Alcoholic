b034f095aeb1efdc655dc485d2af6088f7975dbf
김종덕 <kjd742388@gmail.com>
2024-11-11 09:39:21 +0900

    feat: mobilenet 분류 코드 업로드

---
+++ embedded/RaspberryPi5/mobilenet_classify.py
@@ -0,0 +1,20 @@
+import cv2
+from tensorflow.keras.models import load_model
+
+# MobileNet 모델 로드
+mobilenet_model = load_model('mobilenet_model.h5')  # MobileNet 모델 파일 경로를 지정해야 합니다.
+
+def classify_bottle(bottle_img):
+    """
+    MobileNet 모델을 사용하여 잘라낸 이미지의 술 종류를 분류
+    """
+    # 이미지 전처리
+    bottle_img = cv2.resize(bottle_img, (224, 224))
+    bottle_img = bottle_img / 255.0
+    bottle_img = bottle_img.reshape((1, 224, 224, 3))
+    
+    # MobileNet 모델로 분류 수행
+    prediction = mobilenet_model.predict(bottle_img)
+    class_label = prediction.argmax()  # 예측된 클래스 라벨 반환
+    
+    return class_label
