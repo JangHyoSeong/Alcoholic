a2d6dc52ceea565b2052bb8d1dc2f9f7db41b8cb
JHS <wkdgytmd200@naver.com>
2024-11-26 09:19:31 +0900

    frontend

--- frontend/src/components/home/MyRefBanner.tsx
+++ frontend/src/components/home/MyRefBanner.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
 import tw from 'twrnc';
 import { ImageBackground, TouchableOpacity, View } from 'react-native';
 import CustomFont from '../common/CustomFont';
 import mandrake from '@/assets/mandrake.png'
-import refImage from '@/assets/ref.png'
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { useNavigation } from '@react-navigation/native';
 import { StorageStackParamList } from '@/navigations/stack/StorageStackNavigator';
-import { MyStorageNavigations, mainNavigations } from '@/constants';
-import { useUserStore } from '@/state/useUserStore';
+import { MyStorageNavigations } from '@/constants';
+import { Shadow } from 'react-native-shadow-2'
 
 const MyRefBanner = () => {
   const navigation = useNavigation<NativeStackNavigationProp<StorageStackParamList>>()
 
   const handlePress = () => {
-    navigation.navigate(mainNavigations.MYSTORAGE, {screen: MyStorageNavigations.STORAGE_HOME})
+    navigation.navigate(MyStorageNavigations.STORAGE_HOME)
   }
-  const isRef = useUserStore((state) => state.isRef)
-
-  const backgroundImage = isRef ? refImage : mandrake
-  const bannerText = isRef ? '바로 가기' : '술장고를 등록해 주세요!'
 
  return (
   <TouchableOpacity onPress={handlePress} activeOpacity={0.7}>
@@ -27,11 +21,11 @@ const MyRefBanner = () => {
       <CustomFont style={tw`mt-5 text-[25px] ml-5`}>나의 술장고</CustomFont>
     </View>
     <ImageBackground
-      source={backgroundImage}
+      source={mandrake}
       style={[tw`ml-[7px] mt-5 w-[400px] h-[200px] rounded-[20px] overflow-hidden opacity-80`, {elevation: 10}]}
       resizeMode='cover'>
       <View style={tw`p-4`}>
-        <CustomFont style={tw`text-white text-[28px] ml-10 mt-10`}>{bannerText}</CustomFont>
+        <CustomFont style={tw`text-white text-[28px] ml-10 mt-10`}>술장고를 등록해 주세요!</CustomFont>
       </View>
     </ImageBackground>
   </TouchableOpacity>
